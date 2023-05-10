a035e3f39df8e8d701224b8bd3edbe18f3389ba5
Priyank Shankar <shankarpriyank312002@gmail.com>
2023-05-10 22:10:03 +0530

    Added Search ViewModel

---
+++ tracker/tracker_presentation/src/main/java/com/priyank/tracker_presentation/search/SearchEvent.kt
@@ -0,0 +1,23 @@
+package com.priyank.tracker_presentation.search
+
+import com.priyank.onboarding_domain.model.TrackableFood
+import com.priyank.tracker_domain.model.MealType
+import java.time.LocalDate
+
+sealed class SearchEvent {
+    data class OnQueryChange(val query: String) : SearchEvent()
+    object OnSearch : SearchEvent()
+    data class OnToggleTrackableFood(val food: TrackableFood) : SearchEvent()
+    data class OnAmountForFoodChange(
+        val food: TrackableFood,
+        val amount: String
+    ) : SearchEvent()
+
+    data class OnTrackFoodClick(
+        val food: TrackableFood,
+        val mealType: MealType,
+        val date: LocalDate
+    ) : SearchEvent()
+
+    data class OnSearchFocusChange(val isFocused: Boolean) : SearchEvent()
+}
